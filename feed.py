1c83628e9d482419d28a5b1952dd547c4132deb2
Wollens <zh2017728@163.com>
2020-08-11 22:31:25 +0800

    fetch my blog

--- feed.py
+++ feed.py
@@ -1,4 +1,18 @@
+import requests
+import xml.etree.ElementTree as ET
 
+from feedparser import parse
+
+feed = requests.get('http://www.wollens.top/all/rss/').text
+url = 'http://www.wollens.top/all/rss/'
+data=parse(url)
+entries = data['entries'][:5]
+for entry in entries:
+    title = entry['title']
+    url = entry['link'].replace('http://127.0.0.1:28001', 'http://www.wollens.top')
+
+with open('README.md', 'w') as f:
+    f.write(r'''
     _   _      _                                                                                      _
                       ___====-_  _-====___
             _--^^^#####//      \\#####^^^--_
@@ -18,10 +32,13 @@
                    (vvv(VVV)(VVV)vvv)
 
 ## Latest blog posts
-- [DNS-你想知道的 DNS信息全在这里](http://www.wollens.top/blog/articles/1/)
+''')
+    f.write('- [{}]({})\n'.format(title, url))
 
+    f.write('''
 [🌆 My blog](http://www.wollens.top/blog)
 
 ## Statistics
 ![Stats](https://github-readme-stats.vercel.app/api?username=netcan)
 ![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=netcan&hide=ipynb,html&layout=compact)
+''')
